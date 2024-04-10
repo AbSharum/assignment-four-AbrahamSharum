190203a2cb0a7283b24150629c1c073061fe4541
Abraham Sharum <asharu02@uafs.edu>
2024-04-10 01:51:32 -0500

    more changes

--- views/articleDisplay.php
+++ views/articleDisplay.php
@@ -10,12 +10,12 @@
             for($index=0;$index<count($data);$index++){
                 $userDAO = new UserDAO();
                 $user = $userDAO->getUser($data[$index]->getUserID());
-                echo "<ol><tr><td class=\"text-center\"></td>";
+                echo "<ul><tr><td class=\"text-center\"></td>";
                 echo "<td><strong>".$data[$index]->getTitle()."</strong><br></td>";
                 echo "<td> <img src=".$data[$index]->getImgPath()."><br></td>";
                 echo "<td><i>".$data[$index]->getContent()."</i><br></td><br>";
                 echo "<td> <strong>Made by: </strong> ".$user->getFirstName()." ".$user->getLastName()."<br></td>";
-                echo "<td> <strong>Email: </strong> ".$user->getEmail()."<br></td><br></ol>";
+                echo "<td> <strong>Email: </strong> ".$user->getEmail()."<br></td><br></ul>";
         }
         ?>        
 </body>
